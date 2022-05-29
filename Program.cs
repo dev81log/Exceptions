b39db1d751216f84f9da29e6a10728e9ecc0adab
dev81log <dev81log@gmail.com>
2022-05-29 14:58:05 -0300

    Finalizado estudo basico sobre tratamento de erros no  c#

--- Program.cs
+++ Program.cs
@@ -6,10 +6,35 @@
         {
             var myArray = new int[5];
 
-            for (var index = 0; index < 8; index++)
+            try
             {
-                Console.WriteLine(myArray[index]);
+                // for (var index = 0; index < 8; index++)
+                //     Console.WriteLine(myArray[index]);
+
+                VerificaTexto(false);
             }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.InnerException);
+                Console.WriteLine(exception.Message);
+                Console.WriteLine("Verificou aqui");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Ops...Algo deu errado!");
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Verificado e agora True");
+            }
+        }
+
+        static void VerificaTexto(bool ok)
+        {
+            if (ok == true)
+                Console.WriteLine("Verdadeiro");
+            throw new Exception("Resultado da verificação do texto foi False");
         }
     }
 }
