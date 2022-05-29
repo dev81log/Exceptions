750ff60e211aa8347b615a640b4464863555799e
dev81log <dev81log@gmail.com>
2022-05-29 14:22:50 -0300

    iniciando estudos exceptions

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exceptions
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var myArray = new int[5];
+
+            for (var index = 0; index < 8; index++)
+            {
+                Console.WriteLine(myArray[index]);
+            }
+        }
+    }
+}
